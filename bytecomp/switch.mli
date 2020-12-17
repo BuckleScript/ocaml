ab71dd8a8799eb339c6cf6b52dd2b53d2ec2df3f
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-12-17 09:49:39 +0800

    no offset for bs backend

--- bytecomp/switch.mli
+++ bytecomp/switch.mli
@@ -79,7 +79,7 @@ module type S =
       make_switch arg cases acts
       NB:  cases is in the value form *)
     val make_switch :
-        Location.t -> act -> int array -> act array -> Lambda.switch_names option -> act
+        Location.t -> act -> int array -> act array -> offset:int -> Lambda.switch_names option -> act
    (* Build last minute sharing of action stuff *)
    val make_catch : act -> int * (act -> act)
    val make_exit : int -> act
