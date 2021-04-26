47685e910f39844ccb912a0ca4e811b668079d00
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2021-04-26 17:05:30 +0800

    support removal of int32

--- typing/env.mli
+++ typing/env.mli
@@ -37,8 +37,8 @@ type summary =
 type t
 
 val empty: t
-val initial_safe_string: t
-val initial_unsafe_string: t
+val initial_safe_string: t lazy_t
+
 val diff: t -> t -> Ident.t list
 val copy_local: from:t -> t -> t
 
